c07abb8922a91fa280b944744a17a239c55b5ed3
yaojenkuo <tonykuoyj@gmail.com>
2017-07-19 22:18:58 +0800

    first commit

---
+++ hw3/hw301.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+'''
+建立一個外觀 (9, 9) 的 ndarray 並且填入九九乘法表
+Author: XXX
+'''
---
+++ hw3/hw302.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+'''
+利用 numpy 提供的 .size 屬性、.sum() 方法、.mean() 方法與 .sqrt() 實作樣本標準差函數
+Author: XXX
+'''
