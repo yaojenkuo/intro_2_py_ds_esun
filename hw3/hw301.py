2e904108bda2491c47e8b650ef5c0a918907b584
yaojenkuo <tonykuoyj@gmail.com>
2017-07-27 07:38:29 +0800

    added solutions

--- hw3/hw301.py
+++ hw3/hw301.py
@@ -3,3 +3,23 @@
 建立一個外觀 (9, 9) 的 ndarray 並且填入九九乘法表
 Author: XXX
 '''
+
+# solution 1
+import numpy as np
+
+arr_1 = np.arange(1, 10).reshape(__, __)
+arr_2 = np.arange(1, 10).reshape(__, __)
+print np.dot(arr_1, arr_2)
+
+# solution 2
+import numpy as np
+
+ans = np.empty(__, dtype = 'int').reshape(__, __)
+nrow = ans.shape[0]
+ncol = ans.shape[1]
+
+for i in range(nrow):
+    for j in range(ncol):
+        ans[i, j] = __ * __
+
+print ans
--- hw3/hw302.py
+++ hw3/hw302.py
@@ -3,3 +3,13 @@
 利用 numpy 提供的 .size 屬性、.sum() 方法、.mean() 方法與 .sqrt() 實作樣本標準差函數
 Author: XXX
 '''
+
+import numpy as np
+
+def my_sd(x):
+    x = np.array(x)
+    N_minus_1 = __ - 1
+    x_bar = np.mean(__)
+    sqe = (__ - __)**2
+    sse = np.sum(__)
+    return np.sqrt(__ / __)
